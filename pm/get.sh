f6ad8473b6d1bffd3fda9afbd795fc6d6e318904
Alexander Harvey Nitz <alex.nitz@gmail.com>
2023-05-21 03:09:01 -0400

    update

--- pm/get.sh
+++ pm/get.sh
@@ -1,1 +1,1 @@
-wget --no-check-certificate https://core-gitlfs.tpi.uni-jena.de/core_database/Hyb_0002/-/raw/master/R01/data.h5?inline=false
+wget --no-check-certificate -o data.hdf https://core-gitlfs.tpi.uni-jena.de/core_database/Hyb_0002/-/raw/master/R01/data.h5?inline=false
